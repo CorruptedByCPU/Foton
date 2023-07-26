29771acd732ae1e6a867877c7fc7e8d71028ee28
Andrzej Adamczyk <53617677+CorruptedByCPU@users.noreply.github.com>
2023-07-26 15:26:32 +0200

    script extension

--- qemu.sh
+++ qemu.sh
@@ -3,9 +3,17 @@
 # Copyright (C) Andrzej Adamczyk (at https://blackdev.org/). All rights reserved.
 #=================================================================================
 
+# default CPUs amount: 2
+SMP="${1}"
+if [ -z "${SMP}" ]; then SMP="2"; fi
+
+# default Memory size: 6 MiB
+MEM="${2}"
+if [ -z "${MEM}" ]; then MEM="6"; fi
+
 qemu-system-x86_64			\
 	--enable-kvm			\
 	-cpu host			\
-	-smp 2				\
-	-m 8				\
+	-smp ${SMP}			\
+	-m ${MEM}			\
 	-cdrom build/foton.iso		\
