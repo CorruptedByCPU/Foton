adf6f35059ad29c1aeef079f5513d65a789eccea
Andrzej Adamczyk <53617677+CorruptedByCPU@users.noreply.github.com>
2023-11-16 19:51:06 +0100

    update

--- software/free.c
+++ software/free.c
@@ -2,12 +2,75 @@
  Copyright (C) Andrzej Adamczyk (at https://blackdev.org/). All rights reserved.
 ===============================================================================*/
 
+struct STD_STREAM_STRUCTURE_META stream_meta;
+
+struct STD_SYSCALL_STRUCTURE_MEMORY memory;
+
+void unit( uint8_t level ) {
+	// select unit
+	switch( level ) {
+		case 1: { print( "KiB " ); break; }
+		case 2: { print( "MiB " ); break; }
+		case 3: { print( "GiB " ); break; }
+		case 4: { print( "TiB " ); break; }
+		default: 
+			// undefinied
+			print( "Err" );
+	}
+}
+
+void ratio( uint64_t bytes ) {
+	if( bytes >= 0x10000000000 ) { printf( "%4u ", bytes / 0x10000000000 ); unit( 4 ); return; }
+	if( bytes >= 0x40000000 ) { printf( "%4u ", bytes / 0x40000000 ); unit( 3 ); return; }
+	if( bytes >= 0x100000 ) { printf( "%4u ", bytes / 0x100000 ); unit( 2 ); return; }
+	if( bytes >= 0x400 ) { printf( "%4u ", bytes / 0x400 ); unit( 1 ); return; }
+}
+
+void status( uint64_t total, uint64_t available ) {
+	// width of status bar
+	int16_t width = stream_meta.width - (36 + 4);
+	if( width < 10 ) return;	// no enough space to show anything
+
+	// calculate usage in percents
+	uint8_t used = (uint8_t) (((double) (total - available) / (double) total) * (double) width);
+	uint8_t free = ((double) available / (double) total) * (double) width;
+
+	// by default status bar is green
+	print( "\033[38;5;118m" );
+
+	// if there is less than half or quartes space, change color
+	if( available < total >> STD_SHIFT_2 ) print( "\033[38;5;226m" );
+	if( available < total >> STD_SHIFT_4 ) print( "\033[38;5;196m" );
+
+	// show usage
+	while( used-- ) print( "|" );
+
+	// and show left space in default color
+	printf( "\033[38;5;239m" );
+	while( free-- ) print( "|" );
+
+	// and at last %
+	printf( "%3u%%", (uint64_t) (((double) (total - available) / (double) total) * 100.0f) );
+}
+
 int64_t _main( uint64_t argc, uint8_t *argv[] ) {
+	// retrieve stream meta data
+	std_stream_get( (uint8_t *) &stream_meta, STD_STREAM_OUT );
+
 	// retrieve properties of system memory
-	struct STD_SYSCALL_STRUCTURE_MEMORY memory;
 	std_memory( (struct STD_SYSCALL_STRUCTURE_MEMORY *) &memory );
 
-	printf( "available %u", memory.available );
+	// show header
+	print( "            Total     Used     Free\n" );
+
+	//----------------------------------------------------------------------
+
+	// internal memory
+	print( " Memory: " ); ratio( memory.total ); ratio( memory.total - memory.available ); ratio( memory.available );  status( memory.total, memory.available );
+
+	//----------------------------------------------------------------------
+
+	// to be done... all available storages
 
 	// exit
 	return 0;
