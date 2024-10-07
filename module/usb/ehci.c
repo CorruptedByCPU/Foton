768257bb8ac655bc26c500dcfa39de6c05d1541d
Andrzej Adamczyk <53617677+CorruptedByCPU@users.noreply.github.com>
2024-10-07 11:45:04 +0200

    update

---
+++ module/usb/ehci.c
@@ -0,0 +1,6 @@
+/*===============================================================================
+ Copyright (C) Andrzej Adamczyk (at https://blackdev.org/). All rights reserved.
+===============================================================================*/
+
+void module_usb_ehci_init( uint8_t c ) {
+}
